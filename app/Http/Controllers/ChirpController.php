dddb66dae691b7f48a1e04d67b773d555d56f25a
李晏慈 <3a832005@gm.student.ncut.edu.tw>
2022-11-27 22:36:56 +0800

    第三單元Creating Chirps:Blade

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -14,8 +14,8 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        //
-        return 'Hello, World!';
+        //return 'Hello, World!';
+        return view('chirps.index');
     }
 
     /**
